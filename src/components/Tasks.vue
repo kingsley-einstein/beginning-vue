c66a40bb826b5b766fdb0e0a41a7a75297086bf2
Kingsley Victor <javaprodigy56@gmail.com>
2021-03-28 12:12:14 +0100

    init commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="tasks"></div>
+ <div v-for="t in tasks" v-bind:key="t.id">
+  <div>{{t.title}}</div>
+ </div>
+</template>
+<script>
+ export default {
+  name: "Tasks",
+  data: () => ({
+   tasks: [{
+    title: "Play Football",
+    id: 1,
+    done: false
+   }]
+  }),
+  methods: {
+   addTask(task) {
+    this.tasks = this.tasks.concat(task);
+   }
+  },
+  created() {
+   console.log("Component is created");
+  }
+ }
+</script>
